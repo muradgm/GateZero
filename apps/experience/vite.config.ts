6ab298bdf1190c87449369ecc51d894b57e52a77
Murad Muqbel <59296531+muradgm@users.noreply.github.com>
2026-07-20 00:32:50 +0200

    Configure experience Vite build

---
+++ apps/experience/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          three: ["three", "@react-three/fiber", "@react-three/drei"]
+        }
+      }
+    }
+  }
+});
