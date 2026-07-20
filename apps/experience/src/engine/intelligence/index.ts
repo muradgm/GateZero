c28288f0922ebb2395f58923e78780df99b3018d
Murad Muqbel <59296531+muradgm@users.noreply.github.com>
2026-07-20 19:49:03 +0200

    feat(intelligence): expose intelligence engine API

---
+++ apps/experience/src/engine/intelligence/index.ts
@@ -0,0 +1,9 @@
+export { evaluateIntelligence } from "./intelligenceEngine";
+export type {
+  DirectionalBias,
+  IntelligenceCategory,
+  IntelligenceCluster,
+  IntelligenceSignal,
+  IntelligenceState,
+  NormalizedSignal
+} from "./types";
