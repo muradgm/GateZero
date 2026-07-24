14dcb70d243811cb80f5ce8969699495ebcd7b1a
Murad Muqbel <59296531+muradgm@users.noreply.github.com>
2026-07-25 01:41:04 +0200

    feat: export decision pipeline services

--- packages/application/src/index.ts
+++ packages/application/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./build-trading-intelligence-report.js";
 export * from "./create-setup-review.js";
+export * from "./decision-pipeline.js";
 export * from "./evaluate-setup-review.js";
 export * from "./file-setup-review-repository.js";
 export * from "./in-memory-setup-review-repository.js";
